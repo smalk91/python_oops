00a74968350a00ece213ec65c3029cd23118751d
Chinmay <malkhandi.chinmay@gmail.com>
2022-09-22 22:58:01 +0530

    Adding a test code which contains the python syntax of defining classes and objects

---
+++ basic_oops.py
@@ -0,0 +1,36 @@
+class Car:
+    def __init__(self, model, price, yearm):
+        self.model = model
+        self.price = price
+        self.yearm = yearm
+
+    def increase_price(self):
+        self.price = int(self.price * 1.1)
+
+
+honda = Car("City", 10, 2008)
+tata = Car("Harrier", 15, 2020)
+
+print("tata.model : ", tata.model)
+print("tata.price : ", tata.price)
+print("tata.yearm : ", tata.yearm)
+
+print("*" * 50)
+
+print("honda.model : ", honda.model)
+print("honda.price : ", honda.price)
+print("honda.yearm : ", honda.yearm)
+
+print("*" * 50)
+tata.increase_price()
+
+
+print("tata.model : ", tata.model)
+print("tata.price : ", tata.price)
+print("tata.yearm : ", tata.yearm)
+
+print("*" * 50)
+
+print("honda.model : ", honda.model)
+print("honda.price : ", honda.price)
+print("honda.yearm : ", honda.yearm)
